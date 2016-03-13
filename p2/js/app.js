9ad8f187a5f2af8470df9509edfd86f05a4a290b
Joe Zhang <zhangyijia006@hotmail.com>
2016-03-13 14:34:06 -0400

    test routing

--- p2/js/app.js
+++ p2/js/app.js
@@ -1,7 +1,21 @@
-var app = angular.module('coding', []);
+var app = angular.module('coding', ['ngRoute']);
 
    
-
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/project-detail', {
+                templateUrl: 'project-detail.html',
+                controller: 'MainController'
+            }).
+            // when('/products/:productId', {
+            //     templateUrl: 'partials/detail.html',
+            //     controller: 'DetailCtrl'
+            // }).
+            otherwise({
+                redirectTo: '/project-detail'
+            });
+    }]);
 
 
 
